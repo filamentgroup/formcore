09366a8de6696037223c1afcc36473795ccda80c
Jeffrey Lembeck <jlembeck@gmail.com>
2014-07-10 17:28:53 -0700

    initial build pieces

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+/*
+ * formcore
+ * https://github.com/filamentgroup/formcore
+ *
+ * Copyright (c) 2014 Filament Group
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    }
+
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint']);
+
+};
