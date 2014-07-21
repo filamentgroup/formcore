8219cb5fe766e4652e7632c062c7ab38cd3a951a
Maggie Wachs <maggie@filamentgroup.com>
2014-07-21 15:48:15 -0400

    added password mask behavior/style

---
+++ js/password-init.js
@@ -0,0 +1,44 @@
+/*
+ * Password-input functions
+ *
+ * Copyright (c) 2014 Filament Group, Inc.
+ * Licensed under MIT
+ */
+(function( $, window ) {
+
+  var pluginName = "mask-pw",
+      initSelector = "[data-" + pluginName + "]";
+
+  $.fn[ pluginName ] = function(){
+    return this.each(function(){
+        var el = this,
+            $el = $( this ),
+            $tog = $( "<b class='button btn-small btn-mask-pw'>"),
+            $newinp;
+          
+        function toggleMask() {
+          var $inp = $el.find( "input" ),
+              type = $inp.attr( "type" );
+
+          $newinp = $inp.clone();
+          $newinp[ 0 ].value = $inp.val();
+          $newinp[ 0 ].type = type === "text" ? "password" : "text";
+          $tog.html( ( type === "text" ? "show" : "hide" ) + " password" );
+          $inp.replaceWith( $newinp );
+        }
+          
+        toggleMask();
+        $tog.insertBefore( $newinp );
+
+        $tog.bind( "click", function(){
+          toggleMask();
+        });
+    });
+  };
+
+  // auto-init on enhance (which is called on domready)
+  $( document ).bind( "enhance", function( e ){
+    $( initSelector, e.target )[ pluginName ]();
+  });
+
+}( jQuery, this ));
