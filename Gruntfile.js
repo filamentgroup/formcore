de4c7caeccd7cb93e662d6bef8ebf53167e2deee
Jeffrey Lembeck <jlembeck@gmail.com>
2015-04-03 13:05:39 -0700

    We tend to use tabs here

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,24 +10,24 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    }
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: [
+				'Gruntfile.js'
+			],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		}
 
-  });
+	});
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('stage', ['default']);
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('stage', ['default']);
 
 };
