38e43fb733193a90f43a54b05c2f4f513aadbde5
Jeffrey Lembeck <jlembeck@gmail.com>
2015-04-09 09:30:48 -0700

    numeric fun

--- js/numeric-input.js
+++ js/numeric-input.js
@@ -10,6 +10,28 @@
 
 	NumericInput.prototype.init = function(){
 		this.el.pattern = this.pattern;
+
+		this.$el.on( "keydown", function( event ){
+
+			var handled = false;
+			if (event.key !== undefined) {
+				//Handle the event with KeyboardEvent.key and set handled true.
+				console.log( "Key" );
+				console.log( event.key );
+			} else if (event.keyIdentifier !== undefined) {
+				// Handle the event with KeyboardEvent.keyIdentifier and set handled true.
+				console.log( "Key Identifier" );
+				console.log( event.keyIdentifier );
+			} else if (event.keyCode !== undefined) {
+			// Handle the event with KeyboardEvent.keyCode and set handled true.
+				console.log( "Key Code" );
+				console.log( event.keyCode );
+			}
+			if (handled) {
+				// Suppress "double action" if event handled
+				event.preventDefault();
+			}
+		});
 	};
 
 	window.NumericInput = NumericInput;
