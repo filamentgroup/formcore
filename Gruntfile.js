1a5f69eb8f210b335828f01e5a8b8fade6556b5f
Zach Leatherman <zachleatherman@gmail.com>
2016-04-19 09:34:53 -0500

    Move dependencies to dist/dependencies.[js|css]

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,15 +19,39 @@ module.exports = function(grunt) {
 			options: {
 				jshintrc: '.jshintrc'
 			}
+		},
+		concat: {
+			options: {},
+			js: {
+				src: [
+					'node_modules/shoestring/dist/shoestring.js',
+					'node_modules/xrayhtml/dist/xrayhtml.js',
+					'node_modules/validator/dist/validator.js',
+					'node_modules/validator/dist/validator.config.js',
+					'node_modules/validator/src/validator.page.js',
+					'node_modules/validator/src/validator.page-init.js',
+					'node_modules/validator/dist/validator-init.js'
+				],
+				dest: 'dist/dependencies.js'
+			},
+			css: {
+				src: [
+					'node_modules/xrayhtml/dist/xrayhtml.css',
+					'node_modules/validator/dist/validator.css',
+					'node_modules/fg-select-css/src/select-css.css',
+					'node_modules/fg-select-css/src/select-css-compat.css'
+				],
+				dest: 'dist/dependencies.css'
+			}
 		}
-
 	});
 
 	// These plugins provide necessary tasks.
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 
 	// By default, lint and run all tests.
-	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('default', ['jshint', 'concat']);
 	grunt.registerTask('stage', ['default']);
 
 };
