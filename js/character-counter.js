2c645b95b9035aaf628f6fbf60b25d42c019278a
Zach Leatherman <zachleatherman@gmail.com>
2016-04-19 13:30:24 -0500

    Adds character counter

---
+++ js/character-counter.js
@@ -0,0 +1,47 @@
+// Shows the number of characters left when typing in a textarea
+;(function( w ){
+	var $ = w.jQuery;
+
+	function CharacterCounter( el ) {
+		this.characterCounter.call( el );
+	}
+
+	CharacterCounter.prototype.characterCounter = function() {
+		var $label = $( "[data-maxlength-count='" + this.id + "'],[data-minlength-count='" + this.id + "']" ),
+			$parent = $label.parent().addClass( "character-counter plural" ),
+			max = parseFloat( $( this ).attr( "maxlength" ) ),
+			min = parseFloat( $( this ).attr( "minlength" ) ),
+			newval;
+
+		if ( $label.length ){
+			$( this ).unbind( ".charcount") .bind( "input.charcount keyup.charcount", function(){
+				newval = ( max || min ) - this.value.length;
+				$label.html( "" + newval );
+				var add = [];
+				var remove = [ "limit" ];
+				if( newval !== 1 ) {
+					remove.push( "singular" );
+					add.push( "plural" );
+				} else {
+					add.push( "singular" );
+					remove.push( "plural" );
+				}
+				var hitMax = max && newval === 0;
+				var hitMin = min && newval < 0;
+				if( hitMax || hitMin ){
+					add.push( "limit " + ( hitMax ? "max" : "min" ) );
+					$label.html( "0" );
+				}
+				$parent.removeClass( remove.join( " " ) ).addClass( add.join( " " ) );
+			});
+		}
+	};
+
+	$( w.document ).bind( "enhance", function( e ) {
+		//textarea max-length counter
+		$( e.target ).find( "textarea[maxlength],textarea[minlength]" ).each(function(){
+			new CharacterCounter( this );
+		});
+	});
+
+}( typeof global !== "undefined" ? global : this ));
