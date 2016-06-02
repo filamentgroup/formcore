7433bc4bb6687461df005c4325065e4bf4bca2bf
John Bender <john.m.bender@gmail.com>
2016-06-02 11:27:59 -0400

    first hack

--- js/character-counter.js
+++ js/character-counter.js
@@ -6,6 +6,14 @@
 		this.characterCounter.call( el );
 	}
 
+  var length = function( e ){
+    if( /Safari|Chrome/.test(window.navigator.userAgent) ){
+      return e.value.replace(/[\n\r]/g, "__").length
+    }
+
+    return e.value.length;
+  }
+
 	CharacterCounter.prototype.characterCounter = function() {
 		var $label = $( "[data-maxlength-count='" + this.id + "'],[data-minlength-count='" + this.id + "']" ),
 			$parent = $label.parent().addClass( "character-counter plural" ),
@@ -15,7 +23,7 @@
 
 		if ( $label.length ){
 			$( this ).unbind( ".charcount") .bind( "input.charcount keyup.charcount", function(){
-				newval = ( max || min ) - this.value.length;
+				newval = ( max || min ) - length(this);
 				$label.html( "" + newval );
 				var add = [];
 				var remove = [ "min max limit" ];
