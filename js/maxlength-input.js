b4731d92e264683393e14fa8ee2e4d64e842e7d8
John Bender <john.m.bender@gmail.com>
2016-06-14 13:13:08 -0400

    refine the maxlength removal so other plugins can continue to use it on enhance

--- js/maxlength-input.js
+++ js/maxlength-input.js
@@ -10,26 +10,34 @@
 		this.maxlength = this.$el.attr("maxlength");
 
 		this.$el
+			.bind("focus", function(){
+				// remove the maxlength constraint on focus, after other plugins
+				// have had a chance to select on/store the value but before
+				// the constraint can be enforced on paste or value entry
+				self.el.removeAttribute("maxlength");
+
+				// record the value for reference
+				self.el.setAttribute("data-formcore-maxlength", self.maxlength);
+			})
 			.bind("keydown", function(event){
+				// prevent extra text entry based on the maxlength
 				self.onKeydown(event);
 			})
 			.bind("paste", function(event){
+				// prevent extra characters in the form element from pastes
 				self.onPaste(event);
-			})
-
-		this.el.removeAttribute("maxlength");
-		this.el.setAttribute("data-formcore-maxlength", this.maxlength);
+			});
 	}
 
 	MaxlengthInput.allowedKeys = [
-		9, // Tab
+		9,  // Tab
 		13, // Enter
-		27, //Escape
-		8, // Backspace
+		27, // Escape
+		8,  // Backspace
 		39, // ArrowRight
 		37, // ArrowLeft
 		38, // ArrowUp
-		40 // ArrowDown
+		40  // ArrowDown
 	];
 
 	MaxlengthInput.prototype.isKeyAllowed = function( event ) {
