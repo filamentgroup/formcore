4b21671bd940c2cee87c9380714af41e4e875692
John Bender <john.m.bender@gmail.com>
2016-08-01 15:36:57 -0400

    basic disclosure tests

--- test/disclosure.js
+++ test/disclosure.js
@@ -1,17 +1,32 @@
 (function( w, $ ) {
 	"use strict";
 	var $simple;
+	var $disclosed;
+	var $form;
 
 	function commonSetup(){
 		$(document).trigger("enhance");
 		$simple = $( "#simple" );
+		$disclosed = $( "#disclosed" );
+		$form = $( "form" );
 	}
 
 	module( "Methods", {
 		setup: commonSetup
 	});
 
-	test("truth", function(){
-		ok($simple.length);
+	test("reveals disclosure target", function(){
+		ok( $disclosed.is( ".hidden" ), "is hidden" );
+		$simple[0].checked = true;
+		$form.trigger( "change" );
+		ok( !$disclosed.is( ".hidden" ), "is not hidden" );
+	});
+
+	test("hides undisclosed targets", function(){
+		$disclosed.removeClass( "hidden" );
+		ok( !$disclosed.is( ".hidden" ), "is not hidden" );
+		$simple[0].checked = false;
+		$form.trigger( "change" );
+		ok( $disclosed.is( ".hidden" ), "is hidden" );
 	});
 })(window, shoestring);
